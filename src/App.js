fe576c5b2d8197444ab0fff63448553d121f6ce0
Nutn Bhootra <bhootra1993@gmail.com>
2021-05-08 12:05:08 +0530

    123

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-      <HashRouter basename={process.env.PUBLIC_URL}>
           <div>
               <p>There we are</p>
           </div>
-      </HashRouter>
   );
 }
 
