848c8c89357c394c4e2bf957afed068e6c5c9a25
Nutn Bhootra <bhootra1993@gmail.com>
2021-05-09 18:26:47 +0530

    1st draft

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 import Header from './header/Header';
 import Body from './body/Body';
 import Footer from './footer/Footer';
-import { AppBar, Typography, makeStyles } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import HomeIcon from '@material-ui/icons/Home';
 
 function App() {
   return (
           <div className="App">
-
-
-                  <Typography variant="h5"><HomeIcon style={{fontSize: 'x-large', verticalAlign:'top', color:'#000000'}}/></Typography>
+              <Typography variant="h5"><HomeIcon style={{fontSize: 'x-large', verticalAlign:'top', color:'#000000'}}/></Typography>
+              <Typography align="right"><p style={{fontSize:'10px'}}>Author : An Avid Reader, A Proud Memer, A Passionate Coder</p></Typography>
 
               <Header/>
 
