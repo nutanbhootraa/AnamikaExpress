b852774d0b9db6aca224a37f559ab17bbf1eaaf6
Nutn Bhootra <bhootra1993@gmail.com>
2021-05-11 19:57:28 +0530

    1st draft

---
+++ src/blogs/SampleBlog.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class SampleBlog extends Component{
+
+    render() {
+
+
+        return (
+            <div>
+                <p>I am Sample blog</p>
+            </div>
+        );
+    }
+}
+
+export default SampleBlog;
--- src/body/Body.js
+++ src/body/Body.js
@@ -1,21 +1,23 @@
 import './Body.css';
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import Select from "@material-ui/core/Select";
 import Paper from '@material-ui/core/Paper';
-import ALL from './ALL.png';
-import ShortStories from './ShortStories.png';
-import Puzzles from './Puzzles.png';
-import TechBlog from './TechBlog.png';
-import LateNightRandomThoughts from './LateNightRandomThoughts.png';
 
 const useStyles = makeStyles((theme) => ({
     mainClass: {
         boxShadow: theme.shadows[6],
-        backgroundColor:"light yellow",
+        backgroundColor:"light grey",
         padding: theme.spacing(3),
-
+    },
+    sidebarAboutBox: {
+        padding: theme.spacing(2),
+        backgroundColor: theme.palette.grey[200],
+        boxShadow: theme.shadows[6],
     },
 }));
 
@@ -23,41 +25,136 @@ const useStyles = makeStyles((theme) => ({
 function Body() {
     const classes = useStyles();
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [category, setCategory] = React.useState('All');
+
+    const handleClose = () => {
+        setAnchorEl(null);
+        setCategory('All');
+    };
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+        setCategory(event.currentTarget.value);
+    };
+    const listOfCategories = [
+        {
+            id: '1',
+            category: 'All',
+        },
+        {
+            id: '2',
+            category: 'Short Stories',
+        },
+        {
+            id: '3',
+            category: 'Puzzles',
+        },
+        {
+            id: '4',
+            category: 'Tech Blog',
+        },
+        {
+            id: '5',
+            category: 'Random Thoughts',
+        },
+    ];
+
+    const listOfBlogs = [
+        {
+            id: '1',
+            category: 'Short Stories',
+            title: 'Why women can\'t keep secrets',
+        },
+        {
+            id: '2',
+            category: 'Short Stories',
+            title: 'Mental Health : A Luxury Today?',
+        },
+        {
+            id: '3',
+            category: 'Puzzles',
+            title: 'Puzzle 1 : Slitherlink',
+        },
+        {
+            id: '4',
+            category: 'Short Stories',
+            title: 'Missing flight at Mumbai international airport',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: 'My 1st flight journey',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: 'Who am I?',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: '3 mentors of my life',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: 'Let me breathe; please set me free',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: 'Independent India : A crossword puzzle',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: 'Lockdown Thoughts : Privilege of what I want over what I need',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: 'क्या हम वाकई स्वतंत्र हैं',
+        },
+        {
+            id: '5',
+            category: 'Short Stories',
+            title: 'A Thought',
+        },
+    ];
+
     return (
         <div className={classes.mainClass}>
+            <Container maxWidth="xs" align="center" display="inline">
+            <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                style={{backgroundColor:'coral'}}
+            >
+                {category}
+            </Button>
 
-                <Container maxWidth="xs" align="center" display="inline">
-                    <h1>Categories</h1>
-                        <Paper elevation={3} variant='outlined' >
-                            <img src={ALL} alt="ALL" width="150px" height="150px" />
-                        </Paper>
-                    <Paper elevation={3} variant='outlined' >
-                            <img src={ShortStories} alt="ShortStories" width="150px" height="150px" />
-                        <Typography variant="subtitle1" gutterBottom>
-                            Will be released every Friday and Saturday
-                        </Typography>
+            <Menu
+                keepMounted
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                open={Boolean(anchorEl)}
+            >
+                {listOfCategories.map((category) => (
+                    <MenuItem onClick={handleClose}>{category.category}</MenuItem>
+                ))}
+            </Menu>
+            </Container>
 
-                        </Paper>
-                    <Paper elevation={3} variant='outlined' >
-                            <img src={Puzzles} alt="Puzzles" width="150px" height="150px" />
-                        <Typography variant="subtitle1" gutterBottom>
-                            Will be released every Wednesday
-                        </Typography>
+            <Container>
+                {listOfBlogs.map((blog) => (
+                    <Paper elevation={3} className={classes.sidebarAboutBox}>
+                        {blog.title}
+                    </Paper>
+                ))}
+            </Container>
 
-                    </Paper>
-                    <Paper elevation={3} variant='outlined' >
-                            <img src={TechBlog} alt="TechBlog" width="150px" height="150px" />
-                        <Typography variant="subtitle1" gutterBottom>
-                            Will be released every Sunday
-                        </Typography>
-                    </Paper>
-                    <Paper elevation={3} variant='outlined' >
-                            <img src={LateNightRandomThoughts} alt="LateNightRandomThoughts" width="150px" height="150px" />
-                        <Typography variant="subtitle1" gutterBottom>
-                            Will be released every day
-                        </Typography>
-                    </Paper>
-                </Container>
 
         </div>
     );
