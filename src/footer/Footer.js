834abcb5556b34acc7da8b7c48101501cb34b8fb
Nutn Bhootra <bhootra1993@gmail.com>
2021-05-13 16:30:54 +0530

    1st draft

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,4 +1,3 @@
-import './Footer.css';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,15 +8,33 @@ import Paper from '@material-ui/core/Paper';
 
 function Copyright() {
     return (
-        <Typography variant="body2" color="textSecondary">
-            {'Copyright © '}
-            <Link color="inherit" href="https://anamikaexpress.com/">
-                Anamika Express
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
+
+        <Container maxWidth="sm" align="center">
+            <Typography variant="body1"  color="textSecondary"><h6><b>THIS BLOG IS COLLECTION OF AUTHOR'S THOUGHTS.IT IS NOT INTENDED TO HURT ANYONE’S FEELINGS.</b></h6></Typography>
+
+            <Typography variant="body2" color="textSecondary">
+                {'Copyright © '}
+                <Link color="inherit" href="https://anamikaexpress.com/">
+                    Anamika Express
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        </Container>
     );
+}
+
+function Author(classes) {
+    return <Container maxWidth="sm" align="center">
+        <Grid item xs={12} md={4}>
+            <Paper elevation={0} className={classes.sidebarAboutBox}>
+                <Typography variant="h6" gutterBottom>
+                    Author :
+                </Typography>
+                <Typography>An Avid Reader <br/> A Proud Memer<br/> A Passionate Coder</Typography>
+            </Paper>
+        </Grid>
+    </Container>
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -29,11 +46,11 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(3, 2),
         marginTop: 'auto',
         backgroundColor:
-            theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[800],
     },
     sidebarAboutBox: {
         padding: theme.spacing(2),
-        backgroundColor: theme.palette.grey[200],
+        backgroundColor: theme.palette.grey[50],
         boxShadow: theme.shadows[1],
     }
 }));
@@ -46,21 +63,8 @@ function Footer() {
     return (
         <div className={classes.root}>
             <footer className={classes.footer}>
-                <Container maxWidth="sm" align="center">
-                    <Grid item xs={12} md={4}>
-                        <Paper elevation={0} className={classes.sidebarAboutBox}>
-                            <Typography variant="h6" gutterBottom>
-                                Author :
-                            </Typography>
-                            <Typography>An Avid Reader <br/> A Proud Memer<br/> A Passionate Coder</Typography>
-                        </Paper>
-                    </Grid>
-                </Container>
-                <Container maxWidth="sm">
-                    <Typography variant="body1"><b>THIS BLOG IS COLLECTION OF AUTHOR'S THOUGHTS.</b></Typography>
-                    <Typography variant="body1"><b>IT IS NOT INTENDED TO HURT ANYONE’S FEELINGS.</b></Typography>
-                    <Copyright />
-                </Container>
+                <Author classes={classes}/>
+                <Copyright />
             </footer>
         </div>
     );
