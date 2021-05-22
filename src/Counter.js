4939bbff6b3328a583f80fcb769d674c1e035fcd
nutanbhootraa <bhootra1993@gmail.com>
2021-05-23 01:09:11 +0530

    add counter

--- src/Counter.js
+++ src/Counter.js
@@ -3,11 +3,12 @@ import React, {useState} from 'react';
 function Counter() {
     return (
         window.location.href.includes('counterNutan') ?
-        <div>
-            <a href='https://www.counter12.com'>
-                <img src='https://www.counter12.com/img-65dYD8a4Z2881wY7-1.gif' border='0' alt='counter12'/>
-            </a><script type='text/javascript' src='https://www.counter12.com/ad.js?id=65dYD8a4Z2881wY7'></script>
-        </div> : null
+            <div align=center>
+                <a href='https://www.counter12.com'>
+                    <img src='https://www.counter12.com/img-by27d33aD834842c-13.gif' border='0' alt='free web counter'/>
+                </a>
+                <script type='text/javascript' src='https://www.counter12.com/ad.js?id=by27d33aD834842c'></script>
+            </div> : null
     );
 }
 
