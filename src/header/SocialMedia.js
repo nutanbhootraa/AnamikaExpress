58a33e0b82b7ddb2d3fa30e906b5a3705852ab36
nutanbhootraa <bhootra1993@gmail.com>
2021-12-12 15:41:24 +0530

    Elementary mathematics

--- src/header/SocialMedia.js
+++ src/header/SocialMedia.js
@@ -5,7 +5,6 @@ function SocialMedia() {
     return (
         <div className="social">
             <SocialIcon url="https://twitter.com/AnamikaExpress" style={{ height: 25, width: 25 }}/>
-            <SocialIcon url="https://facebook.com/anamikaexpress" style={{ height: 25, width: 25 }}/>
             <SocialIcon url="https://www.instagram.com/anamikaexpress" style={{ height: 25, width: 25 }}/>
             <SocialIcon url="https://www.linkedin.com/in/nutan-bhootra-40843851" style={{ height: 25, width: 25 }}/>
         </div>
