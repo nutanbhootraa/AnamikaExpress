ee8ee04db75c635089533a0ad045c3dcb2e73ff2
nutanbhootraa <bhootra1993@gmail.com>
2022-04-23 21:10:04 +0530

    mongo basic crud

--- src/blogs/blogFiles/ThirtyFive.js
+++ src/blogs/blogFiles/ThirtyFive.js
@@ -28,40 +28,61 @@ const ThirtyFive= () => {
                     <ul>
                         <li> show dbs  // shows existing databases on the server.</li>
                         <li>use anamikaexpress // Connects to the database, even if it does not exist, it will create on the fly and connects. But before we enter any data, it won't be shown in show dbs.</li>
-                        <li><pre>{`db.articles.inserOne({"title":"mongoDb", "level":"basic"})`}</pre>
+                        <li> <div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.inserOne({"title":"mongoDb", "level":"basic"})`}</pre> </div>
                         // It will create on the fly, if not created already. We can omit "" from key as long as long there is no whitespace in key name. We can use '' instead of "" in value. it will create one extra column Id whose type will be ObjectId.
                         </li>
+                        <li> <div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.insertMany([{"title":"mongoDb", "level":"basic"}])`}</pre> </div>
+                            // We can insert many documents at once.
+                        </li>
+                        <li> <div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.insert({"title":"mongoDb", "level":"basic"})`}</pre>
+                            <pre>{`db.articles.insert([{"title":"mongoDb", "level":"basic"}])`}</pre>
+                        </div>
+                            // We can insert one or many documents at once.
+                        </li>
                         <li>we can add id manually, but it should be unique. If it's not unique, db will throw an error.
-                            <pre>{`db.articles.inserOne({"_id":"abc","title":"mongoDb", "level":"basic"})`}</pre>
+                            <div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                                <pre>{`db.articles.inserOne({"_id":"abc","title":"mongoDb", "level":"basic"})`}</pre></div>
                         </li>
                         <li>db.articles.find({}) // gives back cursor object which has all the documents in the database.</li>
                         <li>db.articles.find({}).pretty() // shows in a pretty way.</li>
 
                         <li>db.articles.find({}).toArray() // stores in an array.</li>
-                        <li><pre>{`db.articles.find({}).forEach((article) => {printjson(article.author)}))`}</pre></li>
-                        <li><pre>{`db.articles.find({},{title:1})`}</pre> // it will give only id and title column. 1 denotes show the column.</li>
-                        <li><pre>{`db.articles.find({},{title:1, "_id": 0})`}</pre> // it will give only title column. 0 denotes don't shoe the column.</li>
-                        <li><pre>{`db.articles.find({})`}</pre></li>
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.find({}).forEach((article) => {printjson(article.author)}))`}</pre></div></li>
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.find({},{title:1})`}</pre></div> // it will give only id and title column. 1 denotes show the column.</li>
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.find({},{title:1, "_id": 0})`}</pre></div> // it will give only title column. 0 denotes don't shoe the column.</li>
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.find({})`}</pre></div></li>
                         <li>db.articles.findOne({}).pretty()  // gives error</li>
 
                         <li>it // Uses cursor for more data. When a lot of data comes in shell, it only shows a few records. we have to type it for more records. Cursors don't exist on delete, update, insert commands.</li>
-                        <li>
-                            <pre>{`db.articles.deleteOne({"_id":"abc"})`}</pre> // it deletes the record where id is abc.
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.deleteOne({"_id":"abc"})`}</pre></div> // it deletes the record where id is abc.
                         </li>
-                        <li>
-                            <pre>{`db.articles.deleteMany({"level":"basic"})`}</pre> // it deletes all the records where level is basic.
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.deleteMany({"level":"basic"})`}</pre></div> // it deletes all the records where level is basic.
                         </li>
-                        <li>
-                            <pre>{`db.articles.updateOne({"_id":"abc"}, {$set : {"author": "Dark Lord"}})`}</pre> // it updates one record where id is abc and adds author field. "set" is a keyword, hence we need to put $ in front of it.
+                        <li> <div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.updateOne({"_id":"abc"}, {$set : {"author": "Dark Lord"}})`}</pre></div> // it updates one record where id is abc and adds author field. "set" is a keyword, hence we need to put $ in front of it.
                         </li>
-                        <li>
-                            <pre>{`db.articles.update({"_id":"abc"}, {$set : {"author": "Dark Lord"}})`}</pre> // it overrides one record where id is abc and adds author field.
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.update({"_id":"abc"}, {$set : {"author": "Dark Lord"}})`}</pre> </div>// it overrides one record where id is abc and adds author field.
                         </li>
-                        <li>
-                            <pre>{`db.articles.updateMany({"level":"basic"}, {$set : {"author": "Dark Lord"}})`}</pre> // it updates all the records where level is basic.
+                        <li><div style={{maxWidth: '250px', overflowX: 'scroll'}}>
+                            <pre>{`db.articles.updateMany({"level":"basic"}, {$set : {"author": "Dark Lord"}})`}</pre> </div>// it updates all the records where level is basic.
                         </li>
                         <li>cls // clears the screen of mongo shell.</li>
                         <li>db.articles.drop() // it will drop the collection articles.</li>
+
+                        <li>use anamikaexpress <br/>
+                            db.dropDatabase()  // It drops databases
+
+                        </li>
                     </ul>
 
                     These are the datatypes supported in mongodb.
