7e08243289c57e452cb48aede1d2dd043ee7fd28
nutanbhootraa <bhootra1993@gmail.com>
2022-12-27 14:42:48 +0530

    grammar correction

--- src/blogs/blogFiles/TwentyFive.js
+++ src/blogs/blogFiles/TwentyFive.js
@@ -8,51 +8,51 @@ const TwentyFive = () => {
 
             <Grid align="left" width="75%">
                 <p>
-                    As a child, I had so many questions, so I used to ask those questions to my mom.
-                  <br/> <br/> Once when I was 4 years old, I asked her, Mom why these stars shine? Being a non-science person, she said grandpa is smiling.
-                    <br/> I pointed to the balcony "but grandpa is smiling in the balcony".
-                    <br/>She replied Mine, not yours.
-                    <br/>I asked her but why? <Twemoji svg text="🤔🤔"/>
-                    <br/>She said because I am telling you. Now go and do your homework.
+                    As a child, I had so many questions, so I used to ask my mom.
+                  <br/> <br/> Once when I was 4 years old, I asked her, 'Mom why do these stars shine?' Being a non-science person, she said 'Grandpa is smiling.'
+                    <br/> I pointed to the balcony and said 'but grandpa is smiling in the balcony.'
+                    <br/>She replied, 'Mine, not yours.'
+                    <br/>I asked her 'but why?' <Twemoji svg text="🤔🤔"/>
+                    <br/>She said, 'Because I am telling you. Now go and do your homework.'
                     <br/>I did not believe that, but you can't argue when she is the only person who is feeding you.
-                   <br/> <br/>Cut to when I was 6. Once Teacher said never lie. But he never gave us a reason, why we should never lie <Twemoji svg text="🤷‍♀️🤷‍♀️"/>.
+                   <br/> <br/>Cut to when I was 6. Once my teacher said 'Never lie.' But he never gave us a reason why we should never lie <Twemoji svg text="🤷‍♀️🤷‍♀️"/>.
                    <br/> I came home and asked mom why we should never lie?
-                    <br/>She said if you lie God will punish you.
-                    <br/>I asked but why and how God will know?
-                    <br/>She replied I don't know all that because I am telling you, you must speak the truth.
+                    <br/>She said, 'If you lie, God will punish you.'
+                    <br/>I asked, 'But why and how God will know?'
+                    <br/>She replied, 'I don't know all that because I am telling you, you must speak the truth.'
                     <br/>This time, I was not satisfied with that logic.
+
                    <br/> <br/>I went to my grandma.
-                    <br/>I asked her why we should only speak the truth, how God will know and why he will punish?
+                    <br/>I asked her why we should only speak the truth, how God will know, and why he will punish?
                     <br/>She was a very spiritual person.
                     <br/>
                     <br/>She held me in her lap and told me a small mythological tale:
                     <i>
-                    <br/>In Mahabharata, there was a person named Yudhishthira. No matter what, He never spoke a lie, that's why his chariot used to be 1 foot above the ground. Once in the battleground, when nobody could kill guru Dronacharya, Lord Krishna suggested Pandavas, go to guru Dronacharya and tell him, his son died. that's when he will go to dhyan-mudra to confirm whether his son died or not.
+                    <br/>In Mahabharata, there was a person named Yudhishthira. No matter what, He never spoke a lie, that's why his chariot used to be 1 foot above the ground. Once in the battleground, when nobody could kill guru Dronacharya, Lord Krishna suggested that the Pandavas go to guru Dronacharya and tell him, his son died. That's when he would go into dhyan-mudra to confirm whether his son died or not.
                     <br/>
-                        A question arrived: why would he believe Pandavas, they were his opponents.
+                        A question arose: why would he believe the Pandavas, they were his opponents.
                     <br/>Krishna suggested Yudhishthira should go and tell guru Dronacharya. He will definitely believe Yudhisthira.
                     <br/>Yudhisthira denied going because it was against his ethics. Finally, Yudhisthira reluctantly went and said :
                     <br/>
                     अश्वथामा मारा गया ।
                     <br/> नारो वा कुंजर: ।।
                     <br/> which means Ashwathama died, but I don't know whether human or elephant, there was an elephant too named Ashwathama. As he lied, his chariot came on the ground. This is how God will punish you.
-                    <br/>
-                    </i>
+                    <br/></i>
                     <br/>This time I had 2 more questions.
-                    <br/>1) If God himself told him to lie, then why Yudhisthira was punished? This is cheating <Twemoji svg text = "😡😡"/>
+                    <br/>1) If God himself told him to lie, then why was Yudhisthira punished? This is cheating <Twemoji svg text = "😡😡"/>
                     <br/>2) I don't have a chariot, can I lie then?  <Twemoji svg text = "🤗🤗"/>
-                    <br/>She thought for a while, then she said, I don't know. Go and ask your grandpa.
+                    <br/>She thought for a while and then said, 'I don't know. Go and ask your grandpa.'
                     <br/>
-                    <br/>I went to him and told the whole back story, what maa said, what grandma said.
-                    <br/>He was a very logical person. He said if you tell the truth to many people then you don't have to remember what you said to whom because you will have only one truth. But if you lie, then you will have multiple lies and then you have to remember what you told whom. For your own mental peace, you should only speak the truth.
+                    <br/>I went to him and told him the whole backstory, what maa said, what Grandma said.
+                    <br/>He was a very logical person. He said, 'If you tell the truth to many people then you don't have to remember what you said to whom because you will have only one truth. But if you lie, then you will have multiple lies and you will have to remember what you told whom. For your own mental peace, you should only speak the truth.
                     <br/>
-                    <br/>I had 1 last question: What if I tell the same lie to everyone, this way I don't have to remember. <Twemoji svg text = "🤗🤗"/>
+                    <br/>I had 1 last question: 'What if I tell the same lie to everyone, this way I don't have to remember.' <Twemoji svg text = "🤗🤗"/>
                     <br/>
-                    <br/>He thought for a while and replied there will be someone who will be with you at the time when that event occurred.
+                    <br/>He thought for a while and replied, there will be someone who will be with you at the time when that event occurred.
                     <br/>
-                    <br/>I asked what if nobody is there with me.
-                    <br/>He said God is watching all of us.
-                    <br/>I asked again but god cheated Yudhisthira. Why should I trust him?
+                    <br/>I asked, 'what if nobody is there with me?'
+                    <br/>He said, 'God is watching all of us.'
+                    <br/>I asked again, 'But god cheated Yudhisthira. Why should I trust him?'
                     <br/>
                     <br/>He gave the <b style={{color:'blue'}}> <i> "answer of the century"</i> </b> <h3> because I am telling you
                     <Twemoji svg text="🤭🤭"/> </h3>
--- src/blogs/blogFiles/TwentySix.js
+++ src/blogs/blogFiles/TwentySix.js
@@ -9,14 +9,14 @@ const TwentySix = () => {
             <Grid align="left" width="75%">
                 <p>
                     When my brother was 5 years old, he registered his name to give a speech on Jawahar Lal Nehru.
-                    He got a script according to a 5-year-old. He has prepared very hard for several days.
-                    He went on stage. Started the speech. <br/>
-                    <i>Jawahar Lal Nehru.. Jawahar Lal Nehru... Jawahar Lal Nehru.... Thank you.</i> <br/>
+                    He was given a script appropriate to a 5-year-old. He had prepared very hard for several days.
+                    He went on stage and started the speech. <br/>
+                    <i>'Jawahar Lal Nehru.. Jawahar Lal Nehru... Jawahar Lal Nehru.... Thank you.'</i> <br/>
 
-                    This incident stuck with us for the rest of our lives. He worked everyday on his public speaking skills from then onwards.
-                    Now he hosts his college fests and no family function goes well without him. <br/>
+                    This incident has stuck with us for the rest of our lives. He has worked every day on his public speaking skills since then.
+                    Now he hosts college fests and no family function goes well without him. <br/>
 
-                    Our entire family is really proud of him and his transformation from <b>"Shy" <Twemoji svg text="😫"/></b> to <b>"Secure and confident"<Twemoji svg text="🔥"/></b>.
+                    Our entire family is really proud of his transformation from being <b>"Shy" <Twemoji svg text="😫"/></b> to being <b>"Secure and confident"<Twemoji svg text="🔥"/></b>.
                     <br/><br/>
                     #shortStories #Blogger #Blogging #shyToSecure #myBrother #transformationOfMyBrother
                 </p>
