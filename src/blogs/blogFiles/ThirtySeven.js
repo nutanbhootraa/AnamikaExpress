199ada431a3ed4e3c331f101458a3321590382cb
nutanbhootraa <bhootra1993@gmail.com>
2023-03-19 18:41:59 +0530

    add book review

--- src/blogs/blogFiles/ThirtySeven.js
+++ src/blogs/blogFiles/ThirtySeven.js
@@ -4,45 +4,50 @@ import {Twemoji} from 'react-emoji-render';
 
 const ThirtySeven= () => {
     return (
-        <div>
+        <div align="left">
 
             <p>
                 Have you ever felt like a man who had been thrown from a cliff, whirled in a centrifuge and spat out over a waterfall that fell and fell into emptiness and emptiness and never-quite-touched-bottom-never-never-quite-no not quite- touched - bottom... and you fell so fast you didn't touch the sides either...never...touched...anything
-                <br/> <br/>
+                <br/>
 
-                The same nothingness Guy Montag felt once.<br/> <br/>
+                <b>The same nothingness Guy Montag felt once.</b> <br/>
 
-                Summary : <br/> <br/>
+                Summary : <br/>
                 Fahrenheit 451 : the temperature at which books burn. the story of Guy Montag, a fireman who burns books and book-keepers' houses in a post-literate future where printed books are burnt and walls are made of screens. The book is divided into three parts.
-                <br/> <br/>
-                Part 1 begins with Guy burning books and a house, after which he meets a girl named Clarisse who makes him realize the emptiness in his life. He goes home to find that his wife Mildred has taken sleeping pills. He admits her to the hospital, but the next day she does not remember anything. Their relationship is also empty. A mechanical hound follows Guy, and he feels that the hound does not like him. He goes to the firehouse, where Captain Beatty is waiting for him to complete another task. They go to a woman's place and find books, which they burn. The woman refuses to come out and sets herself on fire. Montag steals a book and comes home. Feeling guilty, he shows Mildred all the books he stole from other people as a souvenir. Mildred panics and suggests burning them, revealing that she has burned a few without Guy's knowledge.
-                <br/><br/>
-                Part 2 begins with Guy searching for the meaning of books and eventually finding Faber, a retired English professor. This arc explores how Faber and Montag make a plan to escape with the books Montag has, but eventually Montag is hunted down by Captain Beatty.
-                <br/><br/>
-                Part 3 explores how Montag burns his own house, kills Beatty and the hound, and escapes from the police. The police fake Montag's death, and he meets ex-firemen who have faced a similar situation. They have photographic memories and remember exactly what they read when they escaped. Together, they try to preserve the knowledge that came with books.
+                <br/>
+                <b>Part 1</b> begins with Guy burning books and a house, after which he meets a girl named Clarisse who makes him realize the emptiness in his life. He goes home to find that his wife Mildred has taken sleeping pills. He admits her to the hospital, but the next day she does not remember anything. Their relationship is also empty. A mechanical hound follows Guy, and he feels that the hound does not like him. He goes to the firehouse, where Captain Beatty is waiting for him to complete another task. They go to a woman's place and find books, which they burn. The woman refuses to come out and sets herself on fire. Montag steals a book and comes home. Feeling guilty, he shows Mildred all the books he stole from other people as a souvenir. Mildred panics and suggests burning them, revealing that she has burned a few without Guy's knowledge.
+                <br/>
+                <b>Part 2</b> begins with Guy searching for the meaning of books and eventually finding Faber, a retired English professor. This arc explores how Faber and Montag make a plan to escape with the books Montag has, but eventually Montag is hunted down by Captain Beatty.
+                <br/>
+                <b>Part 3</b> explores how Montag burns his own house, kills Beatty and the hound, and escapes from the police. The police fake Montag's death, and he meets ex-firemen who have faced a similar situation. They have photographic memories and remember exactly what they read when they escaped. Together, they try to preserve the knowledge that came with books.
                 <br/><br/>
 
-                <ol> 3 Things which are remarkable :
+                <ol> Three Things which are <b>remarkable</b> :
                     <li>The storyline never leaves you without questions. Be it what happened to Clarisse(The girl who is more mature than her own age)? or where did the mechanical hound go?
                         How did Beatty found out about Montag's book collection? How empty people are, for eg. Mrs phelps who is in her 3rd marriage and does not care if her husband dies, because they are independent(of feelings)
                     </li>
                     <li> The power of 3 :
-                        <ol>
+                        <ul>
                 <li>3 parts of the book </li>
                 <li>3 women who had an impact on Guy : Mildred, because of whom he felt lonely & empty. Clarisse, who showed him true himself. Woman on fire (literally), who gave him courage to stand against the system.</li>
                             <li>3 things that books gave the world : Quality of information, Leisure to digest it, and the right to carry out actions based on what we learn from the interaction of the first two.</li>
-                        </ol>
+                        </ul>
                         </li>
                     <li> The ex-firemen still kept Shakespeare, Jonathan Swift, Charles Darwin, etc., alive. It gives hope that when AI takes over the world, there will be a few ex-firemen who will keep humanity and books alive.
                     </li>
                 </ol>
-                <br/>
-                The only thing I did not like was the loose threads (or I felt so). the ex-firemen still kept Shakespeare, Jonathan Swift, Charles Darwin, etc., alive. It gives hope that when AI takes over the world, there will be a few ex-firemen who will keep humanity and books alive.
-<br/> <br/>
+
+                <ol>
+                    The only thing I <b>did not like</b> was two loose threads (or I felt so).
+                    <li>When Mildred found out book in pillow, what happens next? because she is shocked when Guy showed her all the books.</li>
+                    <li>When Captain Beatty was telling a lot of quoted then nobody asked him how he knows a lot of quotes</li>
+                </ol>
+<br/>
                 <b> Overall Rating 4.5/5 </b>
                 <br/>
-                The few quotes I liked :
+
                 <ol>
+                    <b> The few quotes I liked : </b>
                     <li>Everyone is using someone else's coattails</li>
                     <li>Trying to fil a sieve with sand</li>
                     <li>There is nothing magical in books, magic lies in what books say</li>
